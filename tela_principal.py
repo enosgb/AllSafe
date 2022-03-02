099f507174426c54b8ffd1347cae124b10057ef6
Enos Gabriel <64032529+hun251@users.noreply.github.com>
2022-03-02 13:25:43 -0300

    editando template

--- tela_principal.py
+++ tela_principal.py
@@ -1,5 +1,5 @@
 from cgitb import text
-from tkinter import font
+from tkinter import Button, font
 import PySimpleGUI as sg
 from img_tags import *
 from armazenamento import *
@@ -8,24 +8,7 @@ from armazenamento import *
 
 sg.theme('BlueMono')
 layout = [[sg.Button('', image_data=shield, button_color=('light blue', sg.theme_background_color()),
-                      border_width=0, ),sg.Text('Drive Status', font='Helvetica 16 bold italic',text_color='white')]]
-
-
-# Add a row for every partician that has a bar graph and text stats
-particians = psutil.disk_partitions()
-for count, part in enumerate(particians):
-    mount = part[0]
-    try:
-        bar_color = sg.theme_progress_bar_color()
-        this_color = BAR_COLORS[count % len(BAR_COLORS)]
-        usage = psutil.disk_usage(mount)
-        stats_info = f'{human_size(usage.used)} / {human_size(usage.total)} = {human_size(usage.free)} free'
-        layout += [[sg.Text(mount, size=(5, 1), key=('-NAME-', mount)),
-                    sg.ProgressBar(100, 'h', size=(10, 15), key=('-PROG-', mount), bar_color=(this_color, bar_color[1])),
-                    sg.Text(f'{usage.percent}%', size=(6, 1), key=('-%-', mount)), sg.T(stats_info, size=(30, 1), key=('-STATS-', mount))]]
-    except:
-        pass
-    layout += [[sg.Text('Refresh', font='Any 8', key='-REFRESH-', enable_events=True), sg.Text('❎', enable_events=True, key='Exit Text')]]
+                      border_width=0, ),sg.Button('', image_data=UNIFESO,button_color=('light blue',sg.theme_background_color()),border_width=0)]]
 layout += [[sg.Text('Full Scan               ',font='Helvetica 16 bold italic',text_color='white'),sg.Button(' Iniciar Verificação', image_data=image_large_rectangle,font='Helvetica 12 bold italic', button_color=('light blue', sg.theme_background_color()),
                       border_width=0, )],
          [sg.Text('Verificar Arquivo  ',font='Helvetica 16 bold italic',text_color='white'),sg.Button('Selecionar Arquivo', image_data=image_large_rectangle, font='Helvetica 12 bold italic', button_color=('light blue', sg.theme_background_color()),
@@ -35,22 +18,6 @@ layout += [[sg.Text('Full Scan               ',font='Helvetica 16 bold italic',t
                       border_width=0, ),],
          
 ]
-'''
-particians = psutil.disk_partitions()
-for count, part in enumerate(particians):
-    mount = part[0]
-    try:
-        bar_color = sg.theme_progress_bar_color()
-        this_color = BAR_COLORS[count % len(BAR_COLORS)]
-        usage = psutil.disk_usage(mount)
-        stats_info = f'{human_size(usage.used)} / {human_size(usage.total)} = {human_size(usage.free)} free'
-        layout += [[sg.Text(mount, size=(5, 1), key=('-NAME-', mount)),
-                    sg.ProgressBar(100, 'h', size=(10, 15), key=('-PROG-', mount), bar_color=(this_color, bar_color[1])),
-                    sg.Text(f'{usage.percent}%', size=(6, 1), key=('-%-', mount)), sg.T(stats_info, size=(30, 1), key=('-STATS-', mount))]]
-    except:
-        pass
-    layout += [[sg.Text('Refresh', font='Any 8', key='-REFRESH-', enable_events=True), sg.Text('❎', enable_events=True, key='Exit Text')]]
-'''
 
 window = sg.Window('AllSafe Antivirus',layout)
 
