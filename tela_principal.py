abf9dcab2687fe0a076c46b52f448e9ca31e9896
Enos Gabriel <64032529+hun251@users.noreply.github.com>
2022-03-02 15:17:13 -0300

    crianto funções

--- tela_principal.py
+++ tela_principal.py
@@ -1,15 +1,14 @@
-from cgitb import text
-from tkinter import Button, font
 import PySimpleGUI as sg
 from img_tags import *
 from armazenamento import *
+from tela_tarefas import tela_tarefas
 
 
-
-sg.theme('BlueMono')
-layout = [[sg.Button('', image_data=shield, button_color=('light blue', sg.theme_background_color()),
+def main():
+    sg.theme('BlueMono')
+    layout = [[sg.Button('', image_data=shield, button_color=('light blue', sg.theme_background_color()),
                       border_width=0, ),sg.Button('', image_data=UNIFESO,button_color=('light blue',sg.theme_background_color()),border_width=0)]]
-layout += [[sg.Text('Full Scan               ',font='Helvetica 16 bold italic',text_color='white'),sg.Button(' Iniciar Verificação', image_data=image_large_rectangle,font='Helvetica 12 bold italic', button_color=('light blue', sg.theme_background_color()),
+    layout += [[sg.Text('Full Scan               ',font='Helvetica 16 bold italic',text_color='white'),sg.Button(' Iniciar Verificação', image_data=image_large_rectangle,font='Helvetica 12 bold italic', button_color=('light blue', sg.theme_background_color()),
                       border_width=0, )],
          [sg.Text('Verificar Arquivo  ',font='Helvetica 16 bold italic',text_color='white'),sg.Button('Selecionar Arquivo', image_data=image_large_rectangle, font='Helvetica 12 bold italic', button_color=('light blue', sg.theme_background_color()),
                       border_width=0, )],
@@ -19,10 +18,14 @@ layout += [[sg.Text('Full Scan               ',font='Helvetica 16 bold italic',t
          
 ]
 
-window = sg.Window('AllSafe Antivirus',layout)
+    window = sg.Window('AllSafe Antivirus',layout)
 
-while True:
-    event, Values = window.read()
-    if event == sg.WIN_CLOSED:
-        break
-window.close()
+    while True:
+        event, Values = window.read()
+        if event == sg.WIN_CLOSED:
+            break
+        if event == 'Tarefas':
+            tela_tarefas()
+    window.close()
+
+main()
--- tela_tarefas.py
+++ tela_tarefas.py
@@ -1,1 +1,16 @@
-'''em produção'''
+from doctest import BLANKLINE_MARKER
+import PySimpleGUI as sg
+
+def tela_tarefas():
+    sg.theme('BlueMono')
+
+    layout = [[sg.Text('Disco Status')]]
+
+    window = sg.Window('Tarefas',layout)
+
+    while True:
+        event, Values = window.read()
+        if event == sg.WIN_CLOSED:
+           break
+    window.close()
+
