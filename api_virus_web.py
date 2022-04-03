0700efe2e2feb066e6c8ad6786798d62688b3058
Enos Gabriel <64032529+hun251@users.noreply.github.com>
2022-04-02 22:14:59 -0300

    Add files via upload

--- api_virus_web.py
+++ api_virus_web.py
@@ -8,15 +8,17 @@ from pprint import pprint
 configuration = cloudmersive_virus_api_client.Configuration()
 configuration.api_key['Apikey'] = 'Your Api Key'
 
-
+{
+  "Url": "https://secure.eicar.org/eicar.com"
+}
 
 # create an instance of the API class
 api_instance = cloudmersive_virus_api_client.ScanApi(cloudmersive_virus_api_client.ApiClient(configuration))
-input_file = 'eicar_com.zip' # file | Input file to perform the operation on.
+input = cloudmersive_virus_api_client.WebsiteScanRequest(url="https://secure.eicar.org/eicar.com") # WebsiteScanRequest | 
 
 try:
-    # Scan a file for viruses
-    api_response = api_instance.scan_file(input_file)
+    # Scan a website for malicious content and threats
+    api_response = api_instance.scan_website(input)
     pprint(api_response)
 except ApiException as e:
-    print("Exception when calling ScanApi->scan_file: %s\n" % e)
+    print("Exception when calling ScanApi->scan_website: %s\n" % e)
